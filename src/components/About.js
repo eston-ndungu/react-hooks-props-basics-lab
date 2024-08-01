7cfeb6420c6847e71eefea6442b23f36b64badf9
ESTON NDUNGU <estonndungu1@gmail.com>
2024-08-01 13:23:35 +0300

    complete lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,24 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About(props) {
+
+  const {bio} = props;
+  let bioContent;
+  if (bio && bio.trim() !== '') {
+    bioContent = <p>{bio}</p>;
+  } else {
+    bioContent = null;
+  }
+
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {bioContent}
+
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
+    <Links github="https://github.com/liza" linkedin="https://www.linkedin.com/in/liza/"/>
     </div>
   );
 }
